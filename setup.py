11232e015e55c2d7e2ef993255dd692af50c17dd
Aong <aong@lovewithfood.com>
2019-11-06 16:10:46 -0800

    Refactor exploratory analysis of passenger survival predictions using ridge logistic regression

--- setup.py
+++ setup.py
@@ -25,5 +25,9 @@ setup(
     author_email='aihui@aihuiong.com',  # Substitute your email
     license='MIT',
     packages=['titanic'],
-    install_requires=['pypandoc>=1.4'],
+    install_requires=['pypandoc>=1.4',
+                        'pytest>=4.3.1',
+                        'pytest-runner>=4.4',],
+    setup_requires=['pytest-runner'],
+    tests_require=[pytest'],
 )
