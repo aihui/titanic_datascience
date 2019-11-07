8606eb7db6d6472d71ddcf3231d37b2897c97e93
Aong <aong@lovewithfood.com>
2019-11-06 16:33:58 -0800

    Add more requires to the file

--- setup.py
+++ setup.py
@@ -20,17 +20,24 @@ setup(
         'Programming Language :: Python :: 3',
     ],
     # Substitute <github_account> with the name of your GitHub account
-    url='https://github.com/aihui/titanic_datascience',
-    author='Aihui',  # Substitute your name
-    author_email='aihui@aihuiong.com',  # Substitute your email
+    url='https://github.com/<github_account>/titanic_datascience',
+    author='Filippo Bovo',  # Substitute your name
+    author_email='filippo@satalia.com',  # Substitute your email
     license='MIT',
     packages=['titanic'],
-    install_requires=['pypandoc>=1.4',
-                        'pytest>=4.3.1',
-                        'pytest-runner>=4.4',
-                        'click>=7.0'],
+    install_requires=[
+        'pypandoc>=1.4',
+        'watermark>=1.8.1',
+        'pandas>=0.24.2',
+        'scikit-learn>=0.20.3',
+        'scipy>=1.2.1',
+        'matplotlib>=3.0.3',
+        'pytest>=4.3.1',
+        'pytest-runner>=4.4',
+        'click>=7.0'
+    ],
     setup_requires=['pytest-runner'],
-    tests_require=[pytest'],
+    tests_require=['pytest'],
     entry_points='''
         [console_scripts]
         titanic_analysis=titanic.command_line:titanic_analysis
